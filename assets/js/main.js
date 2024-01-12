d9a447520332c978123cb359d03cb0df7f70f562
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-01-11 20:23:28 -0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let category = document.getElementById("text");
+    fetch('../../data.json').then((response)=>{
+        response.json().then((data)=>{
+            data.forEach(element => {
+                category.innerHTML += `
+                <div class="record ${element.category}">
+                    <figure>
+                        <img src=${element.icon} alt="loading"/>
+                        <span>${element.category}</span>
+                    </figure>
+                    <p class="marks">${element.score} <span class="text-gray">/100</span></p>
+                </div>
+                `;
+            });
+        });
+    });
+})
